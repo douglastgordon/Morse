6de3910a84a49475561ab393040d73b1cff869a2
Douglas Gordon <douglas.gordon@vroom.com>
2018-06-27 18:55:15 -0400

    fix morse to english

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const alphabet = {
+const morseEnglishDictionary = {
    "-----":"0",
    ".----":"1",
    "..---":"2",
@@ -35,27 +35,55 @@ const alphabet = {
    "-..-":"x",
    "-.--":"y",
    "--..":"z",
-   "/":" ",
    "-·-·--":"!",
    "·-·-·-":".",
    "--··--":","
 };
 
+const englishMorseDictionary = (() => {
+  return Object.entries(morseEnglishDictionary).reduce((acc, entry) => {
+    const [morseCharacter, englishCharacter] = entry;
+    return Object.assign({}, acc, {[englishCharacter]: morseCharacter});
+  }, {});
+})();
+
+const SINGLE_SPACE = " ";
+const DOUBLE_SPACE = "  ";
 
 const morseToEnglish = morseString => {
-  const words = morseString.split("  ");
+  const words = morseString.split(DOUBLE_SPACE);
   return words.map(word => {
-    const characters = word.split(" ");
-    return characters.map(character => alphabet[character]).join("");
-  }).join(" ");
+    const characters = word.split(SINGLE_SPACE);
+    return characters.map(character => morseEnglishDictionary[character]).join("");
+  }).join(SINGLE_SPACE);
 };
 
+const englishToMorse = englishString => {
+  const words = englishString.split(SINGLE_SPACE);
+  return words.map(word => {
+    const characters = word.split("");
+    return characters.map(character => {
+      return englishMorseDictionary[character.toLowerCase()]
+    }).join(SINGLE_SPACE);
+  }).join(DOUBLE_SPACE);
+};
+
+
+// tests
+const morse = ".... .- .-.. .--. -·-·--  -- --- .-. ... .  -.-. --- -.. .  .. ...  -.. .-. .. ...- .. -. --.  -- .  -. ..- - ... -·-·--";
+const english = "Halp! Morse code is driving me nuts!";
+
 const testMorseToEnglish = () => {
-  const morse = ".... .- .-.. .--. -·-·--  -- --- .-. ... .  -.-. --- -.. .  .. ...  -.. .-. .. ...- .. -. --.  -- .  -. ..- - ... -·-·--";
-  const english = "Halp! Morse code is driving me nuts!";
   const functionWorks = morseToEnglish(morse) === english.toLowerCase();
   console.log("morseToEnglish passes test cases:", functionWorks);
   return functionWorks;
 };
 
+const testEnglishToMorse = () => {
+  const functionWorks = englishToMorse(english) === morse;
+  console.log("testEnglishToMorse passes test cases:", functionWorks);
+  return functionWorks;
+};
+
 testMorseToEnglish();
+testEnglishToMorse();
